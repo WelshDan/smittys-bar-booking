118c49ecf8d2c9eba1b618fb80f49444bbb72ddb
WelshDan <>
2023-09-29 07:11:20 +0000

    Relocate login page to correct folder and add to urls, views

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 
-def get_basepage(request):
+def get_base(request):
     return render(request, 'base.html')
 
 
@@ -9,5 +9,8 @@ def get_booktable(request):
     return render(request, 'booktable.html')
 
 
-def get_indexpage(request):
+def get_index(request):
     return render(request, 'index.html')
+
+def get_login(request):
+    return render(request, 'login.html')
--- smittys/urls.py
+++ smittys/urls.py
@@ -20,9 +20,10 @@ from booking import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', views.get_indexpage, name='indexpage'),
+    path('', views.get_index, name='index'),
     path('booktable/', views.get_booktable, name='booktable'),
-    path('basepage/', views.get_basepage, name='basepage')
+    path('base/', views.get_base, name='base'),
+    path('login/', views.get_login, name='login')
 ]
 
 # Admin titles and headings"
