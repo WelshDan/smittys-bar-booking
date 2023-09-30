535f00a93d79fd1bcaa951d6331f99df0b75364f
WelshDan <>
2023-09-30 23:11:34 +0000

    Set up post function and enable to enter a username and password

--- users/views.py
+++ users/views.py
@@ -4,4 +4,16 @@ from django.contrib import messages
 
 
 def login_user(request):
-    return render(request, 'registration/login.html', {})
+    if request.method == "POST":
+        email = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.success(request, ("That did not work! Try again please"))
+            return redirect('login')
+
+    else:
+        return render(request, 'registration/login.html', {})
