238bbc4ad505744a9df4ffc62a737669c38d002b
WelshDan <>
2023-09-30 23:32:01 +0000

    Add messages function

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 
 def login_user(request):
     if request.method == "POST":
-        email = request.POST["username"]
+        username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
