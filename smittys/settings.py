002f3a1e8e1c7b3dd30a43d62fec96faf1b6fc64
WelshDan <>
2023-10-11 13:27:12 +0000

    Deployment error fix

--- smittys/settings.py
+++ smittys/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "8000-welshdan-smittys-bar-boo-scp68zefr7.us2.codeanyapp.com",
