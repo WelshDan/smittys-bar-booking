d1340d92bf3d6b6548454368a5fff45d87e67aad
WelshDan <dan.roberts78@gmail.com>
2024-04-16 12:20:17 +0000

    Moved debug status to env file to improve security

--- smittys/settings.py
+++ smittys/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = [
     "8000-welshdan-smittysbarbook-h2u5ylgzflv.ws-eu110.gitpod.io",
