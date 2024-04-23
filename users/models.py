7b0f5eff1c12e9e361e7a907e399e8d82ffc0fc6
WelshDan <dan.roberts78@gmail.com>
2024-04-23 19:47:56 +0000

    Logout page restyled and users removed from users model file

--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Customers(models.Model):
-    user = models.OneToOneField(User, default=None, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.CharField(primary_key=True, max_length=100)
